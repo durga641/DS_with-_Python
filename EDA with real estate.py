33c8796534d7a36b6e3b1cd23f2327f55c521639
durga641 <37656214+durga641@users.noreply.github.com>
2018-06-06 15:03:41 +0530

    Add files via upload

---
+++ EDA with real estate.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  5 15:24:27 2018
+
+@author: vuppuluri
+"""
+
+import pandas as pd
+from sklearn import tree
+from sklearn import model_selection
+import os
+
+os.chdir("D:\\DS\\Titanic")
+titanic_train = pd.read_csv("titanic_train.csv")
+
+
+plt.scatter(A,B)
+plt.xscale('log')
+plt.plot(A,B)
+plt.show()
